42e6e73b12a696e3439acf6784bdb98c574f3a95
Ha Yen <hanguyentieuyen@gmail.com>
2022-06-19 14:05:18 +0700

    update

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,5 @@
 import TopNav from "../components/TopNav";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import "../public/css/style.css";
 import Footer from "../components/Footer"
 function MyApp({Component, pageProps}){
